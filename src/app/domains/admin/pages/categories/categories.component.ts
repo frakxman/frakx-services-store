02485596b6686ddf2f69808643e6ffe24b4f24f1
Fabian Rivera <frakxman@hotmail.com>
2026-09-01 20:40:34 -0500

    Implement categories component: add HTML structure, integrate category service, and display category images and names

--- src/app/domains/admin/pages/categories/categories.component.ts
+++ src/app/domains/admin/pages/categories/categories.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+
+import { Category } from '@shared/models/category.model';
+import { CategoryService } from '@shared/services/category.service';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export default class CategoriesComponent {
 
+  categories = signal<Category[]>([]);
+
+  private categoryService = inject(CategoryService);
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  private getCategories() {
+    this.categoryService.getCategories()
+      .subscribe({
+        next: (categories) => {
+          this.categories.set(categories);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+  }
 }
